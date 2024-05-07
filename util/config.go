b2f11515c9eadb8c30fee7faed3c2a901b5c929e
indramhaarta <indramhrt@gmail.com>
2024-05-07 19:35:30 +0700

    ci: add github action script

--- util/config.go
+++ util/config.go
@@ -1,8 +1,6 @@
 package util
 
 import (
-	"time"
-
 	"github.com/spf13/viper"
 )
 
@@ -10,9 +8,6 @@ type Config struct {
 	DBDriver            string        `mapstructure:"DB_DRIVER"`
 	DBSource            string        `mapstructure:"DB_SOURCE"`
 	ServerAddress       string        `mapstructure:"BACKEND_SERVER_ADDRESS"`
-	StorageUrl          string        `mapstructure:"STORAGE_SERVER_URL"`
-	TokenSymmetricKey   string        `mapstructure:"TOKEN_SYMMETRIC_KEY"`
-	AccessTokenDuration time.Duration `mapstructure:"ACCESS_TOKEN_DURATION"`
 	RabbitSource        string        `mapstructure:"RABBIT_SOURCE"`
 	BackendSwaggerHost  string        `mapstructure:"BACKEND_SWAGGER_HOST"`
 }
